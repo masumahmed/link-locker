98bde61fe4f42cecc1c8e12c37f0e1552f75bb80
Masum Ahmed <masumahmed1798@gmail.com>
2023-01-22 02:24:19 -0500

    folder

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ document.getElementById('button').addEventListener("click", function () {
                 'Content-Type': 'application/json'
             }
         })
-            .then(response => response.json())
-            .then(data => console.log(data))
+            .then(response => response.text())
+            .then(data => displayLink(data))
             .catch(error => console.error(error));
 
     } else {
@@ -29,8 +29,40 @@ document.getElementById('button').addEventListener("click", function () {
                 'Content-Type': 'application/json'
             }
         })
-            .then(response => response.json())
-            .then(data => console.log(data))
+            .then(response => response.text())
+            .then(data => (window.location.assign("http://" + data)))
             .catch(error => console.error(error));
     }
 });
+
+function displayLink(hash) {
+    document.querySelector('#copy-output').value = "http://127.0.0.1:3001/?" + hash;
+    document.querySelector('#copy-output').classList.remove('hide');
+    document.querySelector('#password-input').classList.add('hide');
+    document.querySelector('#url-input').classList.add('hide');
+    document.querySelector('#button').classList.add('hide');
+    document.querySelector('#copy-button').classList.remove('hide');
+
+    // Get the text field
+    let copyText = document.querySelector("#copy-output");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+}
+
+function copy() {
+    // Get the text field
+    let copyText = document.querySelector("#copy-output");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    // setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
