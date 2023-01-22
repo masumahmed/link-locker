a26b642c71bd346d318c088ec387097fdbb91238
Masum Ahmed <masumahmed1798@gmail.com>
2023-01-22 02:32:55 -0500

    toast

--- script.js
+++ script.js
@@ -64,5 +64,5 @@ function copy() {
 
     var x = document.getElementById("snackbar");
     x.className = "show";
-    // setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
