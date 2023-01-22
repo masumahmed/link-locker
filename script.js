0c057962b6afe8d21caeee87bd93b63e7a16d573
Masum Ahmed <masumahmed1798@gmail.com>
2023-01-22 00:22:28 -0500

    all

---
+++ script.js
@@ -0,0 +1,36 @@
+let hash = window.location.search.substring(1, window.location.search.length);
+let encoder = true;
+
+if (hash != "") {
+    document.querySelector('#url-input').classList += 'hide';
+    document.querySelectorAll('br')[0].classList += 'hide';
+    encoder = false;
+}
+
+document.getElementById('button').addEventListener("click", function () {
+    let url = document.querySelector('#url-input').value;
+    let password = document.querySelector('#password-input').value;
+
+    if (encoder == true) {
+        fetch('http://127.0.0.1:3000/encode/' + url + '/' + password, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+
+    } else {
+        fetch('https://127.0.0.1:3000/decode/' + hash + '/' + password, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+});
---
+++ server.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto');
+
+const encode = (string, password) => {
+    const algorithm = 'aes-256-cbc';
+    const key = crypto.createHash('sha256').update(password).digest();
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let encrypted = cipher.update(string, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    const hash = iv.toString('hex') + ':' + encrypted;
+    return hash;
+}
+
+const decode = (hash, password) => {
+    const algorithm = 'aes-256-cbc';
+    const key = crypto.createHash('sha256').update(password).digest();
+    const parts = hash.split(':');
+    const iv = Buffer.from(parts.shift(), 'hex');
+    const encrypted = Buffer.from(parts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/encode/:url/:password', async (req, res) => {
+    try {
+        const URL = req.params.url
+        const password = req.params.password
+        res.send(encode(URL, password));
+        console.log("working");
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+app.get('/decode/:hash/:password', (req, res) => {
+    try {
+        const Hash = req.params.hash
+        const password = req.params.password
+        res.send(decode(Hash, password));
+    } catch (err) {
+        console.log(err)
+    }
+});
